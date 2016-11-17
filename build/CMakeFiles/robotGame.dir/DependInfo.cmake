a9b058c5b72d7c7ce12edfd0d4d12e0f6494fb26
TeoKa <matteo_ciocca@yahoo.it>
2016-11-17 23:09:15 +0100

    added CMake to compile

---
+++ build/CMakeFiles/robotGame.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/matteo_ciocca/git_projects/robot-game/src/enemy.cpp" "/Users/matteo_ciocca/git_projects/robot-game/build/CMakeFiles/robotGame.dir/src/enemy.cpp.o"
+  "/Users/matteo_ciocca/git_projects/robot-game/src/main.cpp" "/Users/matteo_ciocca/git_projects/robot-game/build/CMakeFiles/robotGame.dir/src/main.cpp.o"
+  "/Users/matteo_ciocca/git_projects/robot-game/src/robot.cpp" "/Users/matteo_ciocca/git_projects/robot-game/build/CMakeFiles/robotGame.dir/src/robot.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/robotGame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robotGame.dir/src/enemy.cpp.o"
+  "CMakeFiles/robotGame.dir/src/main.cpp.o"
+  "CMakeFiles/robotGame.dir/src/robot.cpp.o"
+  "robotGame.pdb"
+  "robotGame"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robotGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
